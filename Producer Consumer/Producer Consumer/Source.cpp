7d3478a915f4ce794eff4b6a2d6bae4bd07f46c1
C00196266 <c00196266@itcarlow.ie>
2018-01-18 09:58:14 +0000

    Consumer Producer

--- Producer Consumer/Producer Consumer/Source.cpp
+++ Producer Consumer/Producer Consumer/Source.cpp
@@ -1,29 +1,77 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
+#include <mutex>
+
+int counter = 0;
+std::mutex m_lock;
 
 int buf;
 int p = 0;
 int c = 0;
-const int n = 20;
+const int n = 5;
 
-class PC {
-	int count;
-	PC() : count(0) {}
-	void push(void *);
-	void *pop();
-};
+void producer() {
+	int a[n] = { 10, 20, 30, 40, 50 };
+	bool running = true;
 
-//{ PC : c <= p <= c+1 && a[0:n-1] && (p == c + 1) }
+	while (running == true) {
+		while (p < n) {
+			std::cout << "ID: " << std::this_thread::get_id() << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(2));
 
-void Producer() {
-	int a[n];
-
-	if (c <= p <= c + 1 && a[0] && (p == c + 1)) {
-
+			// await p = c
+			while (p != c) {
+				std::cout << "Producer spinning:" << std::endl;
+			}
+			buf = a[p];
+			std::cout << a[p] << " deposited in buffer" << std::endl;
+			p++;
+		}
+		std::cout << "Production finished" << std::endl;
+		running = false;
 	}
+}
 
+void consumer() {
+	int b[n];
+	bool running = true;
+
+	while (running == true) {
+		while (c < n) {
+			std::cout << "ID: " << std::this_thread::get_id() << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+
+			// await p > c
+			while (p <= c) {
+				std::cout << "Consumer spinning:" << std::endl;
+			}
+			b[c] = buf;
+			std::cout << buf << " deposited in b[]" << std::endl;
+			c++;
+		}
+
+		std::cout << "Consumption finished" << std::endl;
+		running = false;
+	}
+}
+
+void fun() {
+	while (counter <= 20) {
+		m_lock.lock();
+		counter++;
+		std::cout << "Counter: " << counter << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		m_lock.unlock();
+	}
 }
 
 int main() {
-	
+	std::thread t1(producer);
+	std::thread t2(consumer);
+
+	t1.join();
+	t2.join();
+
+	std::cin.get();
 }
